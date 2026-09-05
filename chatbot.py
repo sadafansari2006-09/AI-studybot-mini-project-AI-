129bdfff465c5b42d7623a3b0075de05e6ac2364
sadafansari2006-09 <sadafansari2006@gmail.com>
2026-09-05 13:58:30 +0530

    Chatbot

---
+++ chatbot.py
@@ -0,0 +1,18 @@
+from test_data import subjects
+
+
+def show_menu():
+    print("\n" + "=" * 40)
+    print("           🤖 STUDYBOT")
+    print("        Academic Assistant")
+    print("=" * 40)
+
+    print("1. Ask a question")
+    print("2. View syllabus")
+    print("3. View exams")
+    print("4. View practicals")
+    print("5. View assignments")
+    print("6. Exit")
+
+
+show_menu()
---
+++ test_data.py
@@ -0,0 +1,39 @@
+subjects = {
+    "AI": {
+        "exam": "12 September 2026",
+
+        "syllabus": {
+            "completed": ["Module 1", "Module 2"],
+            "pending": ["Module 3", "Module 4"]
+        },
+
+        "practicals": {
+            "completed": ["Experiment 1"],
+            "pending": ["Experiment 2", "Experiment 3"]
+        },
+
+        "assignments": {
+            "completed": ["Assignment 1"],
+            "pending": ["Assignment 2"]
+        }
+    },
+
+    "DBMS": {
+        "exam": "18 September 2026",
+
+        "syllabus": {
+            "completed": ["Module 1"],
+            "pending": ["Module 2", "Module 3"]
+        },
+
+        "practicals": {
+            "completed": ["Experiment 1"],
+            "pending": ["Experiment 2"]
+        },
+
+        "assignments": {
+            "completed": [],
+            "pending": ["Assignment 1", "Assignment 2"]
+        }
+    }
+}
